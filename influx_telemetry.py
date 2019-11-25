1db9d3887c6efdc80bc36f89170c9a8f792e7349
Malte Schröder <malte@tnxip.de>
2019-11-25 20:26:27 +0100

    use new push api

--- influx_telemetry.py
+++ influx_telemetry.py
@@ -4,8 +4,8 @@ import threading
 import influxdb
 import pyrfc3339
 from datetime import datetime, timezone
-from time import time
-from threading import Timer, Lock
+from time import time, sleep
+from threading import Thread, Lock
 import logging
 from math import isnan
 
@@ -20,7 +20,7 @@ class InfluxTelemetry:
         self.gps = gps
         self.poll_interval = config['interval']
         self.running = False
-        self.timer = None
+        self.thread = None
         self.watchdog = time()
         self.watchdog_timeout = self.poll_interval * 10
 
@@ -37,99 +37,111 @@ class InfluxTelemetry:
             Influx = None
 
         self.influx = Influx
+        self.submit_queue = []
+        self.data_q_lock = Lock()
         self.data_queue = []
 
     def start(self):
         if self.influx:
             self.running = True
-            self.timer = Timer(0, self.submitData)
-            self.timer.start()
+            self.thread = Thread(self.submitData)
+            self.thread.start()
+            self.car.registerData(self.dataCallback)
 
     def stop(self):
-        if self.running:
-            self.timer.cancel()
+        self.car.unregisterData(self.dataCallback)
         self.running = False
+        self.data_q_lock.notify()
+        self.thread.join()
+
+    def dataCallback(self, data):
+        with self.data_q_lock:
+            self.data_queue.append(data)
+            self.data_q_lock.notify()
 
     def submitData(self):
-        if not self.running: return
+        while self.running:
+            with self.data_q_lock:
+                if len(self.data_queue) == 0:
+                    self.data_q_lock.wait()
+                else:
+                    data = self.data_queue.pop(0)
+                    now = time()
+                    self.watchdog = now
 
-        now = time()
-        self.watchdog = now
+                    #data = self.car.getData()
+                    if data and 'timestamp' in data:
+                        tags = {
+                                "cartype": self.cartype,
+                                "akey": self.evn_akey,
+                                }
 
-        data = self.car.getData()
-        if data and 'timestamp' in data:
-            tags = {
-                    "cartype": self.cartype,
-                    "akey": self.evn_akey,
-                    }
+                        fields = {}
+                        if 'SOC_DISPLAY' in data:
+                            fields.update({
+                                'SOC_DISPLAY': data['SOC_DISPLAY'],
+                                })
+                        if 'SOC_BMS' in data:
+                            fields.update({
+                                'SOC_BMS': data['SOC_BMS'],
+                                })
 
-            fields = {}
-            if 'SOC_DISPLAY' in data:
-                fields.update({
-                    'SOC_DISPLAY': data['SOC_DISPLAY'],
-                    })
-            if 'SOC_BMS' in data:
-                fields.update({
-                    'SOC_BMS': data['SOC_BMS'],
-                    })
+                        if 'EXTENDED' in data:
+                            fields.update(data['EXTENDED'])
+                        if 'ADDITIONAL' in data:
+                            fields.update(data['ADDITIONAL'])
 
-            if 'EXTENDED' in data:
-                fields.update(data['EXTENDED'])
-            if 'ADDITIONAL' in data:
-                fields.update(data['ADDITIONAL'])
+                        fix = self.gps.fix()
+                        if fix and fix.mode > 1:
+                            fields.update({
+                                    'latitude':  float(fix.latitude),
+                                    'longitude': float(fix.longitude),
+                                    'gdop':      float(fix.gdop),
+                                    'pdop':      float(fix.pdop),
+                                    'hdop':      float(fix.hdop),
+                                    'vdop':      float(fix.vdop),
+                                    'distance':  float(fix.distance),
+                                    })
+                            if not isnan(fix.speed):
+                                fields.update({
+                                    'speed':     float(fix.speed),
+                                    })
+                            if fix.mode > 2 and not isnan(fix.altitude):
+                                fields.update({
+                                    'altitude':  float(fix.altitude),
+                                    })
+                            if fix.device:
+                                tags.update({
+                                    'gps_device': fix.device
+                                    })
 
-            fix = self.gps.fix()
-            if fix and fix.mode > 1:
-                fields.update({
-                        'latitude':  float(fix.latitude),
-                        'longitude': float(fix.longitude),
-                        'gdop':      float(fix.gdop),
-                        'pdop':      float(fix.pdop),
-                        'hdop':      float(fix.hdop),
-                        'vdop':      float(fix.vdop),
-                        'distance':  float(fix.distance),
-                        })
-                if not isnan(fix.speed):
-                    fields.update({
-                        'speed':     float(fix.speed),
-                        })
-                if fix.mode > 2 and not isnan(fix.altitude):
-                    fields.update({
-                        'altitude':  float(fix.altitude),
-                        })
-                if fix.device:
-                    tags.update({
-                        'gps_device': fix.device
-                        })
+                self.submit(measurement="telemetry", time=data['timestamp'],
+                        fields=fields, tags=tags)
 
-            self.submit(measurement="telemetry", time=data['timestamp'],
-                    fields=fields, tags=tags)
-
-        # Prime next loop iteration
-        if self.running:
-            runtime = time() - now
-            interval = self.poll_interval - (runtime if runtime > self.poll_interval else 0)
-            self.timer = Timer(interval, self.submitData)
-            self.timer.start()
+            # Prime next loop iteration
+            #if self.running:
+            #    runtime = time() - now
+            #    interval = self.poll_interval - (runtime if runtime > self.poll_interval else 0)
+            #    sleep(interval)
 
 
     def submit(self, measurement, time, tags, fields):
-        self.data_queue.append({
+        self.submit_queue.append({
             "measurement": measurement,
             "time": pyrfc3339.generate(datetime.fromtimestamp(time, timezone.utc)),
             "tags": tags,
-            "fields": {**fields, 'data_queue_len': len(self.data_queue)}
+            "fields": {**fields, 'submit_queue_len': len(self.submit_queue)}
             })
 
         try:
-            self.influx.write_points(self.data_queue)
-            self.data_queue = []
+            self.influx.write_points(self.submit_queue)
+            self.submit_queue = []
         except influxdb.exceptions.InfluxDBClientError as e:
-            self.log.error("InfluxDBClientError qlen({}): code({}) content({}) last_data({})".format(len(self.data_queue), str(e.code), str(e.content), self.data_queue[-1]))
+            self.log.error("InfluxDBClientError qlen({}): code({}) content({}) last_data({})".format(len(self.submit_queue), str(e.code), str(e.content), self.submit_queue[-1]))
             if e.code == 400:
-                self.data_queue = []
+                self.submit_queue = []
         except Exception as e:
-            self.log.error("InfluxTelemetry len({}): {}".format(len(self.data_queue), str(e)))
+            self.log.error("InfluxTelemetry len({}): {}".format(len(self.submit_queue), str(e)))
 
 
     def checkWatchdog(self):
