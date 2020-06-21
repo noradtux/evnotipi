fa56eadd82b98e5b603e84c50ad974ffdfeb23ef
Malte Schröder <malte@tnxip.de>
2020-06-21 12:57:15 +0200

    add metadata to IONIQ_FL_EV

--- car/IONIQ_FL_EV.py
+++ car/IONIQ_FL_EV.py
@@ -1,4 +1,16 @@
 from .KONA_EV import *
 
 class IONIQ_FL_EV(KONA_EV):
-    pass
+    def getBaseData(self):
+        return {
+            "CAPACITY": 38,
+            "SLOW_SPEED": 2.3,
+            "NORMAL_SPEED": 4.6,
+            "FAST_SPEED": 50.0
+        }
+
+    def getABRPModel(self):
+        return 'hyundai:ioniq:19:38:other'
+
+    def getEVNModel(self):
+        return 'IONIQ_FL_EV'
