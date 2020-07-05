d9da21d3a5fc1c0c1dae82dbb1a07905993c5921
Malte Schröder <malte.schroeder@tnxip.de>
2020-07-05 16:36:20 +0200

    Tune performance

--- car/isotp_decoder.py
+++ car/isotp_decoder.py
@@ -19,9 +19,12 @@ class IsoTpDecoder:
             idx = 0
             for field in cdata['fields']:
                 fmt += str(field.get('cnt', '')) + field['format']
+                field['cnt'] = field.get('cnt', 1)
+                field['scale'] = field.get('scale', 1)
+                field['offset'] = field.get('offset', 0)
                 if 'name' in field:
                     field['fmt_idx'] = idx
-                    field['fmt_len'] = len(field['format'] * field.get('cnt', 1))
+                    field['fmt_len'] = len(field['format'] * field['cnt'])
                     idx += field['fmt_len']
 
             self._log.debug("fmt(%s)", fmt)
@@ -35,8 +38,8 @@ class IsoTpDecoder:
             for cmd, cdata in self._fields.items():
                 raw = self._dongle.sendCommandEx(cmd, canrx=cdata['canrx'],
                                                  cantx=cdata['cantx'])
-                self._log.debug("fmt(%s) %i %i raw(%s)", cdata['cmd_format'],
-                                calcsize(cdata['cmd_format']), len(raw), raw.hex())
+                #self._log.debug("fmt(%s) %i %i raw(%s)", cdata['cmd_format'],
+                #                calcsize(cdata['cmd_format']), len(raw), raw.hex())
                 struct = unpack(cdata['cmd_format'], raw)
 
                 for field in cdata['fields']:
@@ -44,17 +47,22 @@ class IsoTpDecoder:
                         name = field['name']
                         fmt_idx = field['fmt_idx']
                         fmt_len = field['fmt_len']
-                        field_cnt = field.get('cnt', 1)
-                        base_idx = field.get('idx', 0)
+                        field_cnt = field['cnt']
+                        if field_cnt > 1:
+                            base_idx = field['idx']
 
                         for idx in range(0, field_cnt):
-                            iname = name.format(base_idx + idx)
+                            if field_cnt > 1:
+                                iname = name.format(base_idx + idx)
+                            else:
+                                iname = name
+
                             if 'lambda' in field:
                                 value = field['lambda'](struct[fmt_idx+idx:fmt_idx+idx+fmt_len])
                             else:
                                 value = struct[fmt_idx + idx]
 
-                            data[iname] = value * field.get('scale', 1) + field.get('offset', 0)
+                            data[iname] = value * field['scale'] + field['offset']
 
         except NoData:
             if not cdata.get('optional', False):
