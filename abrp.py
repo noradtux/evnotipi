d8bc4f5812fbb689414d4ded7a0bb2cdfdab7ba1
Malte Schröder <malte.schroeder@tnxip.de>
2020-07-24 21:39:59 +0200

    improve interval calc

--- abrp.py
+++ abrp.py
@@ -136,9 +136,7 @@ class ABRP:
 
             # Prime next loop iteration
             if self._running:
-                runtime = time() - now
-                interval = self._poll_interval - \
-                    (runtime if runtime > self._poll_interval else 0)
+                interval = self._poll_interval - (time() - now)
                 sleep(max(0, interval))
 
     def get_next_charge(self):
