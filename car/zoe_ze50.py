c0f48c917f8571767c3a1319f78762e79d0b9a15
Malte Schröder <malte.schroeder@tnxip.de>
2020-09-03 18:05:41 +0200

    ZoeZE50: Found out new PIDs for SOC and current

--- car/zoe_ze50.py
+++ car/zoe_ze50.py
@@ -12,12 +12,13 @@ BCB_TX = 0x18dadef1
 CMD_AUX_VOLTAGE = bytes.fromhex('222005')  # EVC
 CMD_CHARGE_STATE = bytes.fromhex('225017')  # BCB 0:Nok;1:AC mono;2:AC tri;3:DC;4:AC bi
 CMD_SOC = bytes.fromhex('229002')  # EVC
-CMD_SOC_BMS = bytes.fromhex('229002')  # LBC
+CMD_SOC_BMS = bytes.fromhex('229001')  # LBC
 CMD_VOLTAGE = bytes.fromhex('229006')
-CMD_BMS_ENERGY = bytes.fromhex('2291C8')  # PR155
+CMD_BMS_ENERGY = bytes.fromhex('229245')  # PR155 geladene Energie
 CMD_ODO = bytes.fromhex('222006')  # EVC
 CMD_NRG_DISCHARG = bytes.fromhex('229245')  # PR047
-CMD_CURRENT = bytes.fromhex('223204')  # EVC <<- BROKEN
+#CMD_CURRENT = bytes.fromhex('2221DA')  # EVC gemessener Strom
+CMD_CURRENT = bytes.fromhex('229257')  # EVC berechneter Strom
 CMD_SOH = bytes.fromhex('223206')  # EVC
 
 Fields = [
@@ -36,7 +37,7 @@ Fields = [
     {'cmd': CMD_SOC, 'canrx': LBC_RX, 'cantx': LBC_TX,
      'fields': (
          {'padding': 3},
-         {'name': 'SOC_DISPLAY', 'width': 2, 'scale': .02},
+         {'name': 'SOC_DISPLAY', 'width': 2, 'scale': .01},
      )
      },
     {'cmd': CMD_SOC_BMS, 'canrx': LBC_RX, 'cantx': LBC_TX,
@@ -54,7 +55,7 @@ Fields = [
     {'cmd': CMD_BMS_ENERGY, 'canrx': LBC_RX, 'cantx': LBC_TX,
      'fields': (
          {'padding': 3},
-         {'name': 'cumulativeEnergyCharged', 'width': 3, 'scale': .001},
+         {'name': 'cumulativeEnergyCharged', 'width': 4, 'scale': .001},
      )
      },
     {'cmd': CMD_ODO, 'canrx': EVC_RX, 'cantx': EVC_TX,
@@ -69,12 +70,12 @@ Fields = [
          {'name': 'cumulativeEnergyDischarged', 'width': 4, 'scale': .001},
      )
      },
-    #{'cmd': CMD_CURRENT, 'canrx': EVC_RX, 'cantx': EVC_TX,
-    # 'fields': (
-    #     {'padding': 3},
-    #     {'name': 'dcBatteryCurrent', 'width': 1, 'scale': 1},
-    # )
-    # },
+    {'cmd': CMD_CURRENT, 'canrx': LBC_RX, 'cantx': LBC_TX,
+     'fields': (
+         {'padding': 3},
+         {'name': 'dcBatteryCurrent', 'width': 2, 'offset': -((2**15) * .01), 'scale': .01},
+     )
+     },
     # {'cmd': CMD_SOH, 'canrx': EVC_RX, 'cantx': EVC_TX,
     #    'fields': (
     #        {'padding': 3},
@@ -103,25 +104,25 @@ class ZoeZe50(Car):
         Car.__init__(self, config, dongle, watchdog, gps)
         self._dongle.set_protocol('CAN_29_500')
 
-        idx = 1
-        for i in range(0x21, 0x28):
-            cmd = bytes.fromhex("2290%02x" % (i))
-            Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
-                           'fields': ({'padding': 3},
-                                      {'name': 'cellVolt%02d' % (idx),
-                                       'width': 2, 'scale': .001})
-                           })
-            idx += 1
+        #idx = 1
+        #for i in range(0x21, 0x28):
+        #    cmd = bytes.fromhex("2290%02x" % (i))
+        #    Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
+        #                   'fields': ({'padding': 3},
+        #                              {'name': 'cellVolt%02d' % (idx),
+        #                               'width': 2, 'scale': .001})
+        #                   })
+        #    idx += 1
 
-        idx = 1
-        for i in range(0x31, 0x3d):
-            cmd = bytes.fromhex("2291%02x" % (i))
-            Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
-                           'fields': ({'padding': 3},
-                                      {'name': 'cellTemp%02d' % (idx),
-                                       'width': 2, 'scale': .1, 'offset': -60})
-                           })
-            idx += 1
+        #idx = 1
+        #for i in range(0x31, 0x3d):
+        #    cmd = bytes.fromhex("2291%02x" % (i))
+        #    Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
+        #                   'fields': ({'padding': 3},
+        #                              {'name': 'cellTemp%02d' % (idx),
+        #                               'width': 2, 'scale': .1, 'offset': -60})
+        #                   })
+        ##    idx += 1
 
         self._isotp = IsoTpDecoder(self._dongle, Fields)
 
@@ -133,7 +134,7 @@ class ZoeZe50(Car):
     @staticmethod
     def get_base_data():
         return {
-            "CAPACITY": 50,
+            "CAPACITY": 52,
             "SLOW_SPEED": 2.3,
             "NORMAL_SPEED": 22.0,
             "FAST_SPEED": 50.0
