8ffea4035c54a58345c141573ff4908b9078c44d
Malte Schröder <malte.schroeder@tnxip.de>
2020-12-01 19:12:33 +0100

    Huaweimon: Add traffic counters

--- huaweimon_ser.py
+++ huaweimon_ser.py
@@ -9,6 +9,24 @@ import datetime
 import pyrfc3339
 import influxdb
 
+net_re = re.compile(r"(?P<ifname>[a-z0-9\.]+):" +
+                    r"\s+(?P<bytes_rx>\d+)" +
+                    r"\s+(?P<packets_rx>\d+)" +
+                    r"\s+(?P<errs_rx>\d+)" +
+                    r"\s+(?P<drop_rx>\d+)" +
+                    r"\s+(?P<fifo_rx>\d+)" +
+                    r"\s+(?P<frame_rx>\d+)" +
+                    r"\s+(?P<compressed_rx>\d+)" +
+                    r"\s+(?P<multicast_rx>\d+)" +
+                    r"\s+(?P<bytes_tx>\d+)" +
+                    r"\s+(?P<packets_tx>\d+)" +
+                    r"\s+(?P<errs_tx>\d+)" +
+                    r"\s+(?P<drop_tx>\d+)" +
+                    r"\s+(?P<fifo_tx>\d+)" +
+                    r"\s+(?P<colls_tx>\d+)" +
+                    r"\s+(?P<carrier_tx>\d+)" +
+                    r"\s+(?P<compressed_tx>\d+)")
+
 # load config
 if os.path.exists('config.json'):
     import json
@@ -39,10 +57,23 @@ if __name__ == "__main__":
         line = ser.readline()
         if line[:6] == b'^RSSI:':
             now = time.time()
+
             data = {
-                'Strength': int(line[6:])
+                'Strength': int(line[6:]),
                 }
-            now = time.time()
+
+            with open('/proc/net/dev', 'r') as netdev:
+                for line in netdev:
+                    m = net_re.match(line)
+                    if m['ifname'] == 'wwan0':
+                        for key in ['bytes_rx', 'packets_rx', 'errs_rx', 'drop_rx',
+                                    'fifo_rx', 'frame_rx', 'multicast_rx',
+                                    'bytes_tx', 'packets_tx', 'errs_tx', 'drop_tx',
+                                    'fifo_tx', 'colls_tx', 'carrier_tx']:
+                            data[key] = m[key]
+
+                        break
+
             data_queue.append({
                 'measurement': 'mobile_net',
                 'tags': {'akey': C['evnotify']['akey'], 'car': C['car']['type']},
@@ -50,6 +81,7 @@ if __name__ == "__main__":
                 'time': pyrfc3339.generate(
                     datetime.datetime.fromtimestamp(now, datetime.timezone.utc)),
                 })
+
             if now - last_transmit > 10:
                 last_transmit = now
                 try:
