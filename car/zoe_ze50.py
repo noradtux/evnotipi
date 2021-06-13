32fb748c549d249dd39b74ec62cf1a1c70433188
Malte Schröder <malte.schroeder@tnxip.de>
2021-06-13 12:21:40 +0200

    ZoeZE50: Use new simple mode

--- car/zoe_ze50.py
+++ car/zoe_ze50.py
@@ -22,79 +22,47 @@ CMD_CURRENT = bytes.fromhex('229257')  # EVC berechneter Strom
 CMD_SOH = bytes.fromhex('223206')  # EVC
 
 Fields = [
-    {'cmd': CMD_AUX_VOLTAGE, 'canrx': EVC_RX, 'cantx': EVC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'auxBatteryVoltage', 'width': 2, 'scale': .01},
-     )
-     },
-    {'cmd': CMD_CHARGE_STATE, 'canrx': BCB_RX, 'cantx': BCB_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'charge_state', 'width': 1},
-     )
-     },
-    {'cmd': CMD_SOC, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'SOC_DISPLAY', 'width': 2, 'scale': .01},
-     )
-     },
-    {'cmd': CMD_SOC_BMS, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'SOC_BMS', 'width': 2, 'scale': .01},
-     )
-     },
-    {'cmd': CMD_VOLTAGE, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'dcBatteryVoltage', 'width': 4, 'scale': .001},
-     )
-     },
-    {'cmd': CMD_BMS_ENERGY, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'cumulativeEnergyCharged', 'width': 4, 'scale': .001},
-     )
-     },
-    {'cmd': CMD_ODO, 'canrx': EVC_RX, 'cantx': EVC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'odo', 'width': 3},
-     )
-     },
-    {'cmd': CMD_NRG_DISCHARG, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'cumulativeEnergyDischarged', 'width': 4, 'scale': .001},
-     )
-     },
-    {'cmd': CMD_CURRENT, 'canrx': LBC_RX, 'cantx': LBC_TX,
-     'fields': (
-         {'padding': 3},
-         {'name': 'dcBatteryCurrent', 'width': 2, 'offset': (2**15) * .01, 'scale': -0.01},
-     )
-     },
-    # {'cmd': CMD_SOH, 'canrx': EVC_RX, 'cantx': EVC_TX,
-    #    'fields': (
-    #        {'padding': 3},
-    #        {'name': 'soh', 'format': 'b'},
-    #        )
-    #    },
-    {'computed': True,
-     'fields': (
-         {'name': 'dcBatteryPower',
-          'lambda': lambda d: d['dcBatteryCurrent'] * d['dcBatteryVoltage'] / 1000.0},
-         {'name': 'charging',
-          'lambda': lambda d: int(d['charge_state'] != 0)},
-         {'name': 'normalChargePort',
-          'lambda': lambda d: int(d['charge_state'] in (1, 2, 4))},
-         {'name': 'rapidChargePort',
-          'lambda': lambda d: int(d['charge_state'] == 3)},
-     )
-     },
-]
+        {'cmd': CMD_AUX_VOLTAGE, 'canrx': EVC_RX, 'cantx': EVC_TX, 'simple': True,
+            'fields': ({'name': 'auxBatteryVoltage', 'scale': .01})},
+        {'cmd': CMD_CHARGE_STATE, 'canrx': BCB_RX, 'cantx': BCB_TX, 'simple': True,
+            'fields': ({'name': 'charge_state'})},
+        {'cmd': CMD_SOC, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'SOC_DISPLAY', 'scale': .01})},
+        {'cmd': CMD_SOC_BMS, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'SOC_BMS', 'scale': .01})},
+        {'cmd': CMD_VOLTAGE, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'dcBatteryVoltage', 'scale': .001})},
+        {'cmd': CMD_BMS_ENERGY, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'cumulativeEnergyCharged', 'scale': .001})},
+        {'cmd': CMD_ODO, 'canrx': EVC_RX, 'cantx': EVC_TX,
+            'fields': (
+                {'padding': 3},
+                {'name': 'odo', 'width': 3},
+                )
+            },
+        {'cmd': CMD_NRG_DISCHARG, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'cumulativeEnergyDischarged', 'scale': .001})},
+        {'cmd': CMD_CURRENT, 'canrx': LBC_RX, 'cantx': LBC_TX, 'simple': True,
+            'fields': ({'name': 'dcBatteryCurrent', 'offset': (2**15) * .01, 'scale': -0.01})},
+        # {'cmd': CMD_SOH, 'canrx': EVC_RX, 'cantx': EVC_TX,
+        #    'fields': (
+        #        {'padding': 3},
+        #        {'name': 'soh', 'format': 'b'},
+        #        )
+        #    },
+        {'computed': True,
+            'fields': (
+                {'name': 'dcBatteryPower',
+                    'lambda': lambda d: d['dcBatteryCurrent'] * d['dcBatteryVoltage'] / 1000.0},
+                {'name': 'charging',
+                    'lambda': lambda d: int(d['charge_state'] != 0)},
+                {'name': 'normalChargePort',
+                    'lambda': lambda d: int(d['charge_state'] in (1, 2, 4))},
+                {'name': 'rapidChargePort',
+                    'lambda': lambda d: int(d['charge_state'] == 3)},
+                )
+            },
+    ]
 
 
 class ZoeZe50(Car):
@@ -103,26 +71,6 @@ class ZoeZe50(Car):
     def __init__(self, config, dongle, watchdog, gps):
         Car.__init__(self, config, dongle, watchdog, gps)
         self._dongle.set_protocol('CAN_29_500')
-
-        #idx = 1
-        #for i in range(0x21, 0x28):
-        #    cmd = bytes.fromhex("2290%02x" % (i))
-        #    Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
-        #                   'fields': ({'padding': 3},
-        #                              {'name': 'cellVolt%02d' % (idx),
-        #                               'width': 2, 'scale': .001})
-        #                   })
-        #    idx += 1
-
-        #idx = 1
-        #for i in range(0x31, 0x3d):
-        #    cmd = bytes.fromhex("2291%02x" % (i))
-        #    Fields.append({'cmd': cmd, 'canrx': 0x18daf1db, 'cantx': 0x18dadbf1,
-        #                   'fields': ({'padding': 3},
-        #                              {'name': 'cellTemp%02d' % (idx),
-        #                               'width': 2, 'scale': .1, 'offset': -60})
-        #                   })
-        ##    idx += 1
 
         self._isotp = IsoTpDecoder(self._dongle, Fields)
 
